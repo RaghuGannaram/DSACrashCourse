508e4f90c31589ac4e22f89ee5a3b5fab60b0bad
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-03-16 08:38:06 +0530

    Update twosum

--- LeetCode/Arrays & Hashing/0001-two-sum.js
+++ LeetCode/Arrays & Hashing/0001-two-sum.js
@@ -13,40 +13,58 @@
  */
 //Bruteforce Time complexity = O(n^2), Space complexity = O(1)
 var twoSum1 = function (nums, target) {
-	for (let i = 0; i < nums.length - 1; i++) {
-		for (let j = i + 1; j < nums.length; j++) {
-			if (nums[i] + nums[j] == target) {
-				return [i, j];
-			}
-		}
-	}
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
 };
 
 //Hashmap using Object
 var twoSum2 = function (nums, target) {
-	let hashMap = {};
+    let hashMap = {};
 
-	for (let i = 0; i < nums.length; i++) {
-		console.log(hashMap);
-		if (target - nums[i] in hashMap) {
-			return [hashMap[target - nums[i]], i];
-		} else {
-			hashMap[nums[i]] = i;
-		}
-	}
+    for (let i = 0; i < nums.length; i++) {
+        console.log(hashMap);
+        if (target - nums[i] in hashMap) {
+            return [hashMap[target - nums[i]], i];
+        } else {
+            hashMap[nums[i]] = i;
+        }
+    }
 };
 
 //hashMap using Map
 var twoSum3 = function (nums, target) {
-	let map = new Map();
-	for (let i = 0; i < nums.length; i++) {
-		console.log(map);
-		if (map.has(target - nums[i])) {
-			return [map.get(target - nums[i]), i];
-		} else {
-			map.set(nums[i], i);
-		}
-	}
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        console.log(map);
+        if (map.has(target - nums[i])) {
+            return [map.get(target - nums[i]), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+};
+
+//Meta programming
+var twoSum4 = function (nums, target) {
+    const createTwoSumFunction = new Function(
+        "nums",
+        "target",
+        `
+        let map = new Map();
+        for (let i = 0; i < nums.length; i++) {
+            let complement = target - nums[i];
+            if (map.has(complement)) return [map.get(complement), i];
+            map.set(nums[i], i);
+        }
+    `
+    );
+
+    return createTwoSumFunction(nums, target);
 };
 
 console.log(twoSum3([10, 15, 30, 20, 5, 6, 7], 50));
