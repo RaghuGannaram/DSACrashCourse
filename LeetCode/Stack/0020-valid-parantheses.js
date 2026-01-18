76c2e3325a2360b22f87958e0970a0e6d7e9f64b
Raghu Gannaram <raghuvarangannaram@gmail.com>
2026-01-18 23:37:38 +0530

    Updated stack

--- LeetCode/Stack/0020-valid-parantheses.js
+++ LeetCode/Stack/0020-valid-parantheses.js
@@ -3,26 +3,50 @@
  * @return {boolean}
  */
 var isValidParantheses = function (s) {
-	let stack = [],
-		bracketPairs = {
-			"(": ")",
-			"{": "}",
-			"[": "]",
-		};
+    let stack = [],
+        bracketPairs = {
+            "(": ")",
+            "{": "}",
+            "[": "]",
+        };
 
-	for (let char of s) {
-		if (!stack.length) {
-			stack.push(char);
-			continue;
-		}
-		if (bracketPairs[stack.at(-1)] == char) {
-			stack.pop();
-		} else {
-			stack.push(char);
-		}
-	}
+    for (let char of s) {
+        if (!stack.length) {
+            stack.push(char);
+            continue;
+        }
+        if (bracketPairs[stack.at(-1)] == char) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
 
-	return stack.length == 0;
+    return stack.length == 0;
+};
+
+var isValid = function (s) {
+    const pair = {
+            "(": ")",
+            "{": "}",
+            "[": "]",
+        },
+        stack = [];
+
+    for (let ch of s) {
+        if (!stack.length) {
+            stack.push(ch);
+            continue;
+        }
+
+        if (pair[stack.at(-1)] === ch) {
+            stack.pop();
+        } else {
+            stack.push(ch);
+        }
+    }
+
+    return !stack.length;
 };
 
 console.log(isValidParantheses("[]()"));
