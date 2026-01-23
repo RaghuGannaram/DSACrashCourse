59dbcfd4dc39664eaec36df9bda1d2af51e8abd5
Raghu Gannaram <raghuvarangannaram@gmail.com>
2026-01-23 21:13:57 +0530

    Updated Trees

--- LeetCode/Trees/0105-construct-binary-tree-from-preorder-inorder.js
+++ LeetCode/Trees/0105-construct-binary-tree-from-preorder-inorder.js
@@ -26,8 +26,10 @@ var buildTree = function (preorder, inorder) {
 
     let root = new TreeNode(preorder[0]);
     let mid = inorder.indexOf(root.val);
+
     root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
     root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+    
     return root;
 };
 
