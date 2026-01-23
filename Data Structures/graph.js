04a83eccb7698cb69728f5a3f327f5ec8a3f84a8
Raghu Gannaram <raghuvarangannaram@gmail.com>
2026-01-23 14:48:31 +0530

    Updated graph data structure

--- Data Structures/graph.js
+++ Data Structures/graph.js
@@ -13,57 +13,55 @@ c---d---e   |
 */
 
 const unWeightedUndirectedGraph = {
-	a: ["b", "c"],
-	b: ["a", "g", "h"],
-	c: ["a", "d"],
-	d: ["c", "e", "f", "h"],
-	e: ["d"],
-	f: ["d", "i"],
-	g: ["b", "i"],
-	h: ["b", "d"],
-	i: ["f", "g"],
+    a: ["b", "c"],
+    b: ["a", "g", "h"],
+    c: ["a", "d"],
+    d: ["c", "e", "f", "h"],
+    e: ["d"],
+    f: ["d", "i"],
+    g: ["b", "i"],
+    h: ["b", "d"],
+    i: ["f", "g"],
 };
 
-//DFS for unWeightedUndirectedGraph;
-
 function dfsTraversal(graph, node) {
-	let current,
-		visited = [],
-		stack = [],
-		result = [];
+    let current,
+        stack = [],
+        visited = new Set();
 
-	stack.push(node);
+    stack.push(node);
 
-	while (stack.length > 0) {
-		current = stack.pop();
-		visited.push(current);
-		if (!result.includes(current)) result.push(current);
-		for (let next of graph[current]) {
-			if (!visited.includes(next)) stack.push(next);
-		}
-	}
+    while (stack.length > 0) {
+        current = stack.pop();
+        visited.add(current);
 
-	return result;
+        for (let next of graph[current]) {
+            if (!visited.has(next)) stack.push(next);
+        }
+    }
+
+    return Array.from(visited);
 }
 
 function bfsTraversal(graph, node) {
-	let current,
-		visited = [],
-		queue = [],
-		result = [];
+    let current,
+        queue = [],
+        visited = new Set();
 
-	queue.push(node);
+    queue.push(node);
 
-	while (queue.length > 0) {
-		current = queue.shift();
-		visited.push(current);
-		if (!result.includes(current)) result.push(current);
-		for (let next of graph[current]) {
-			if (!visited.includes(next)) queue.push(next);
-		}
-	}
+    while (queue.length > 0) {
+        current = queue.shift();
 
-    return result;
+        visited.add(current);
+
+        for (let next of graph[current]) {
+            if (!visited.has(next)) queue.push(next);
+        }
+    }
+
+    return Array.from(visited);
 }
 
+console.log(dfsTraversal(unWeightedUndirectedGraph, "a"));
 console.log(bfsTraversal(unWeightedUndirectedGraph, "a"));
