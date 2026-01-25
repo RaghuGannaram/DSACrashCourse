49a1a8ffee13241c4c1d6ab1a67109db32446fd0
Raghu Gannaram <raghuvarangannaram@gmail.com>
2026-01-25 18:12:04 +0530

    Updated Two Pointers

--- LeetCode/Two Pointers/0125-valid-palindrome.js
+++ LeetCode/Two Pointers/0125-valid-palindrome.js
@@ -12,86 +12,87 @@
  */
 
 var validPalindrome1 = function (s) {
-	let l, r, str;
+    let l, r, str;
 
-	str = s
-		.toLowerCase()
-		.split("")
-		.filter((c) => {
-			return isAlphaNumeric(c);
-		});
-	l = 0;
-	r = str.length - 1;
-	while (l < r) {
-		if (str[l] !== str[r]) {
-			return false;
-		}
-		l++;
-		r--;
-	}
-	return true;
+    str = s
+        .toLowerCase()
+        .split("")
+        .filter((c) => {
+            return isAlphaNumeric(c);
+        });
+    l = 0;
+    r = str.length - 1;
+    while (l < r) {
+        if (str[l] !== str[r]) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
 
-	function isAlphaNumeric(c) {
-		let num = c.charCodeAt();
-		if ((48 <= num && num <= 57) || (65 <= num && num <= 90) || (97 <= num && num <= 122)) return true;
-		else return false;
-	}
+    function isAlphaNumeric(c) {
+        let num = c.charCodeAt();
+        if ((48 <= num && num <= 57) || (65 <= num && num <= 90) || (97 <= num && num <= 122)) return true;
+        else return false;
+    }
 };
 
 var validPalindrome2 = function (s) {
-	let filteredS = s
-		.toLowerCase()
-		.split("")
-		.filter((i) => i.match(/[a-zA-Z0-9]/));
-	return filteredS.join("") == filteredS.reverse().join("");
+    let filteredS = s
+        .toLowerCase()
+        .split("")
+        .filter((i) => i.match(/[a-zA-Z0-9]/));
+    return filteredS.join("") == filteredS.reverse().join("");
 };
 
 var validPalindrome3 = function (s) {
-	let l = 0,
-		r = s.length - 1;
-	while (l < r) {
-		while (l < r && !s[l].match(/[a-zA-Z0-9]/)) l++;
-		while (l < r && !s[r].match(/[a-zA-Z0-9]/)) r--;
-		if (s[l].toLowerCase() != s[r].toLowerCase()) return false;
-		l++;
-		r--;
-	}
+    let l = 0,
+        r = s.length - 1;
+    while (l < r) {
+        while (l < r && !s[l].match(/[a-zA-Z0-9]/)) l++;
+        while (l < r && !s[r].match(/[a-zA-Z0-9]/)) r--;
+        if (s[l].toLowerCase() != s[r].toLowerCase()) return false;
+        l++;
+        r--;
+    }
 
-	return true;
+    return true;
 };
 
 var validPalindrome4 = function (s) {
-	let l = 0,
-		r = s.length - 1;
-	while (l < r) {
-		while (l < r && !isalnum(s[l])) l++;
-		while (l < r && !isalnum(s[r])) r--;
-		if (s[l].toLowerCase() != s[r].toLowerCase()) return false;
-		l++;
-		r--;
-	}
+    let l = 0,
+        r = s.length - 1;
+    while (l < r) {
+        while (l < r && !isalnum(s[l])) l++;
+        while (l < r && !isalnum(s[r])) r--;
+        if (s[l].toLowerCase() != s[r].toLowerCase()) return false;
+        l++;
+        r--;
+    }
 
-	return true;
+    return true;
 
-	function isalnum(c) {
-		let num = c.charCodeAt();
-		if ((48 <= num && num <= 57) || (65 <= num && num <= 90) || (97 <= num && num <= 122)) return true;
-		else return false;
-	}
+    function isalnum(c) {
+        let num = c.charCodeAt();
+        if ((48 <= num && num <= 57) || (65 <= num && num <= 90) || (97 <= num && num <= 122)) return true;
+        else return false;
+    }
 };
 
 var isPalindrome5 = function (s) {
-	let str = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let str = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
-	let l = 0, r = str.length - 1;
+    let l = 0,
+        r = str.length - 1;
 
-	while (l < r) {
-		if (str[l] !== str[r]) return false;
-		l++;
-		r--;
-	}
+    while (l < r) {
+        if (str[l] !== str[r]) return false;
+        l++;
+        r--;
+    }
 
-	return true;
+    return true;
 };
 
 console.log(validPalindrome1("A man, a plan, a canal: Panama"));
